e435602480ea3b5fd3bcf0d7e5f726f15adad201
Ben Bowser <ben.bowser@fisglobal.com>
2020-07-02 10:24:34 -0400

    directive events

--- src/app/favorite.directive.ts
+++ src/app/favorite.directive.ts
@@ -1,10 +1,18 @@
-import { Directive, HostBinding, Input } from '@angular/core';
+import { Directive, HostBinding, Input, HostListener } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
 
 @Directive({
   selector: '[mwFavorite]'
 })
 export class FavoriteDirective {
   @HostBinding('class.is-favorite') isFavorite = true;
+  @HostBinding('class.is-favorite-hovering') hovering = false;
+  @HostListener('mouseenter') onMouseEnter() {
+      this.hovering = true;
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+      this.hovering = false;
+  }
   @Input() set mwFavorite(value) {
     this.isFavorite = value;
   }
